60d99d290a3dc52d26fdb62fe2d8ea76b0d9c1f2
Stephen Luppi <luppi@Stephens-Mac-mini.local>
2021-12-11 14:27:06 -0600

    Rounding Out day 11

--- day11/main.rb
+++ day11/main.rb
@@ -1,25 +1,14 @@
 # Advent of Code - Day 11
 require_relative "../helpers"
 
-def print_map(input)
-  for r in 0..input.length-1 do
-    for c in 0..input[r].length-1 do
-      if input[r][c] == 0
-        print input[r][c].to_s.red
-      else
-        print input[r][c].to_s
-      end
-    end
-    puts
-  end
-end
 def print_hash(points)
   for r in 0..9 do 
     for c in 0..9 do
       # puts "Looking for point #{r},#{c}"
       point = points.find{|p| p[:row]==r && p[:col]==c}
       # puts "trying to print point #{point}"
-      print point[:value]
+      print "#{point[:value]}".red if point[:value]==0
+      print point[:value] unless point[:value]==0
     end
     puts
   end
@@ -47,68 +36,54 @@ def process(ia)
   print_hash(points)
   puts "----"
 
+  flash_count=0
+
   # do increments
-  2.times do |iteration|
+  # 100.times do |iteration|
+  iteration = 0
+  loop do
     points.map!{|p| p[:value]+=1; p}
 
     #perform flashes
     loop do
       points.filter{|p| p[:value]>9 && !p[:flashed]}.each do |to_flash|
-        puts "Flashing point #{to_flash}"
+        # puts "Flashing point #{to_flash}"
         flash_point(to_flash)
+        flash_count+=1
+
         surround_points = points.filter do |p| 
-          [to_flash[:row]-1..to_flash[:row]+1].include?(p[:row]) &&
-            [to_flash[:col]-1..to_flash[:col]+1].include?(p[:col]) &&
+          (to_flash[:row]-1..to_flash[:row]+1).include?(p[:row]) &&
+            (to_flash[:col]-1..to_flash[:col]+1).include?(p[:col]) &&
             !p[:flashed]
         end
 
-        puts "Should also flash #{surround_points}"
+        # puts "Should also flash #{surround_points}"
+        surround_points.each{|p| p[:value]+=1; p}
       end
 
-      break if true
       break if points.none?{|p| p[:value]>9 && !p[:flashed]}
     end
 
+    # puts "----"
+    # puts "Iteration #{iteration} Grid"
+    # print_hash(points)
+    # puts "----"
+
+    iteration += 1
+    break if points.all?{|p| p[:flashed]==true}
+    
+    # Reset having flashed
+    points.map!{|p| p[:flashed]=false;p}
   end
+
   puts "----"
-  puts "Ending Grid"
+  puts "Last Grid"
   print_hash(points)
   puts "----"
 
-  "Done"
+  # flash_count
+  iteration
 end
 
-def old_process(ia)
-  # convert input to hash
-  ia.map! {|l| l.map(&:to_i)}
-  p ia
 
-  flashes = 0
-  1.times do |iteration|
-    ia.map!{|r| r.map!(&:next)}
-
-    for r in 0..ia.length-1 do 
-      puts "Row #{r}"
-      for c in 0..ia[r].length-1 do
-        puts "Column #{c}"
-      end
-    end
-    flashes = ia.reduce(flashes) do |memo, r|
-      puts "Checking Row #{r}"
-      memo += r.reduce(0) do |m, c|
-        puts "Checking if #{c} is > 9"
-        m+=1 if c > 9
-        m
-      end
-      memo
-    end
-    puts "Flashes now at #{flashes}"
-    ia.map!{|r| r.map!{|c| c>9 ? 0 : c}}
-  end
-
-  print_map(ia)
-
-  ia
-end
-
-p "#{process(read_file_and_chomp("test.txt"))}"
+p "#{process(read_file_and_chomp("input.txt"))}"
