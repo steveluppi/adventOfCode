badfdfba28efa80fd3e8c1188c7123afa8151cce
Steve Luppi <sluppi@gmail.com>
2022-12-01 10:21:08 -0600

    Update tempalte

--- 2022/template/main.rb
+++ 2022/template/main.rb
@@ -1,12 +1,18 @@
 # Advent of Code - Day x
 require_relative "../helpers"
 
-
-def process(input)
+def silver(input)
   for i in input
     puts i
   end
 
+  "done"
+end
+
+def gold(input)
+  for i in input
+    puts i
+  end
 
   "done"
 end
@@ -15,13 +21,14 @@ end
 # Main execution
 @input = read_file_and_chomp(
   case ARGV[0]
-when "second"
-  "second.txt"
-when "first"
-  "first.txt"
-else
-  "example.txt"
-end
+  when "silver"
+    "silver.txt"
+  when "gold"
+    "gold.txt"
+  else
+    "example.txt"
+  end
 )
 
-puts "#{process(@input)}"
+puts "Silver: #{silver(@input)}"
+puts "Gold: #{gold(@input)}"
