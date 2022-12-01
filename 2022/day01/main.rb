1b9aa469cb10e89e88149c19d31031d38b045bad
Steve Luppi <sluppi@gmail.com>
2022-12-01 10:17:44 -0600

    day1 part2

--- 2022/day01/main.rb
+++ 2022/day01/main.rb
@@ -1,8 +1,7 @@
-# Advent of Code - Day x
+# Advent of Code - Day 1
 require_relative "../helpers"
 
-
-def process(input)
+def silver(input)
   biggest=0
   sum=0
   for line in input
@@ -13,20 +12,45 @@ def process(input)
     end
     sum+=line.to_i
   end
+
   biggest
 end
 
+def gold(input)
+  sums=[]
+  sum=0
+
+  for line in input
+    if line.empty?
+      sums.append(sum);
+      sum=0
+      next
+    end
+    sum+=line.to_i
+  end
+  sums.append(sum)
+
+  sums.sort!.reverse!
+  
+  sum=0
+  for i in 0..2
+    sum += sums[i]
+  end
+
+  sum
+end
 
 # Main execution
 @input = read_file_and_chomp(
   case ARGV[0]
-when "second"
-  "second.txt"
-when "first"
-  "first.txt"
-else
-  "example.txt"
-end
+  when "second"
+    "second.txt"
+  when "first"
+    "first.txt"
+  else
+    "example.txt"
+  end
 )
 
-puts "#{process(@input)}"
+puts "#{silver(@input)}"
+puts "#{gold(@input)}"
